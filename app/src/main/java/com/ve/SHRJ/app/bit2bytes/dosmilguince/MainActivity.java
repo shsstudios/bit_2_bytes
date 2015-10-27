a9da17f3296e0de13f88ec7259a0eab11f8186a7
Said Halloun Sleiman <Said Halloun Sleiman>
2015-10-27 14:37:25 -0430

    Initial commit

---
+++ app/src/main/java/com/ve/SHRJ/app/bit2bytes/dosmilguince/MainActivity.java
@@ -0,0 +1,54 @@
+package com.ve.SHRJ.app.bit2bytes.dosmilguince;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText megabits;
+    private TextView descarga;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        megabits = (EditText) findViewById(R.id.megabits);
+        descarga = (TextView)  findViewById(R.id.descarga);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void convertir(View view)
+    {
+        descarga.setText(String.valueOf(Float.parseFloat(megabits.getText().toString()) * (15625/128) ));
+    }
+
+
+
+}
